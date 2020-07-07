646dd7ed3260d2c77edcc601cf424925b2b818f8
Ardufl <arthur.duflos@live.fr>
2020-07-07 14:12:04 +0200

    ListViews-v2
    Utilisation de l'objet slide plutôt que l'objet camera

--- ListViews/Widget.js
+++ ListViews/Widget.js
@@ -14,20 +14,22 @@
 // limitations under the License.
 ///////////////////////////////////////////////////////////////////////////
 var slides = [];
-
+var thmbnails =[];
 define([
     'dojo/_base/declare',
     'jimu/BaseWidget',
-    'esri/Camera'
+    'esri/Camera',
+    'esri/Viewpoint',
+    'esri/webscene/Slide'
 ],
 
-function(declare, BaseWidget,Camera) {
+function(declare, BaseWidget,Camera, Viewpoint,Slide) {
   //To create a widget, you need to derive from BaseWidget.
   return declare([BaseWidget], {
     //Please note that the widget depends on the 4.0 API
     baseClass: 'jimu-widget-listviews',
 
-    _getViewCoordinate : function(self, slidesArray){
+    /*_getViewCoordinate : function(self, slidesArray){
         var cam = new Camera({
             heading : this.view.camera.get('heading'),
             tilt : this.view.camera.get('tilt'),
@@ -35,6 +37,8 @@ function(declare, BaseWidget,Camera) {
         }); // création d'un objet caméra (lorsqu'on cliquera sur le bouton)
         slidesArray.push(cam); // ajout de cet objet à la liste des vues
     },
+
+    //slides : this.sceneView.map.presentation.slides.length === 0 ? [] : this.sceneView.presentation.slides.length,
 
     _removeSlide : function(){
     },
@@ -64,6 +68,16 @@ function(declare, BaseWidget,Camera) {
         console.log('NO VIEW SET');
       }
     },
+    /*_displaySlides2 : function(slidesArray){
+      var option = {
+        width:380,
+        height:120
+      };
+
+      var str = '<li class="slide">' +
+              '<div class="slide-div">' +
+    },*/
+
     test : function(){
       console.log('Event worked');
     },
@@ -76,24 +90,67 @@ function(declare, BaseWidget,Camera) {
             //.addEventListener("click", test());
       //      .addEventListener("click",_getViewCoordinate(slides)); // lorsqu'on clique sur le bouton addView, on lance la fonction _getViewCoordinate
       //this._displaySlides(slides);
+      slides = this.sceneView.map.presentation.slides.length === 0 ? [] : this.sceneView.presentation.slides.length;
       console.log('postCreate');
-
+      console.log(slides);
     },
 
     startup: function() {
       this.inherited(arguments);
       //this.mapIdNode.innerHTML = 'sceneView id:' + this.sceneView.map.id;
       this.mapIdNode.innerHTML = '<button id="addView" class = "esri-button">Add View</button><br><br><button id="gotoview" class = "esri-button">Go to View</button>';
+      var url;
+      var urlbis;
       document
-            .getElementById('addView').addEventListener("click",  () => {slides.push(this.sceneView.camera);
-            console.log(slides)});
+            .getElementById('addView').addEventListener("click",  () => {
+              var options = {
+                width:380,
+                height:120
+              };
+              /*
+              slides.push(this.sceneView.camera);
+              //this.sceneView.map.presentation.slides.push(this.sceneView.camera);
+              console.log(slides);
+              //thmbnails.push(this.sceneView.takeScreenshot(options));
+              url = this.sceneView.takeScreenshot(options).then((screenshot) => {
+                urlbis = screenshot.dataUrl;
+                return screenshot.dataUrl;
+              });
+              console.log(url);
+              thmbnails.push(url);
+              console.log(urlbis);
+              this._displaySlides(slides)*/
+              var cam = this.sceneView.camera;
+              var view = new Viewpoint({
+                camera : cam
+              });
+              var thb = this.sceneView.takeScreenshot(options).then((screenshot) => {
+                urlbis = screenshot.dataUrl;
+                return screenshot.dataUrl;
+              });
+              console.log(thb);
+              var slide = new Slide ({
+                viewpoint : view,
+                thumbnail :{
+                  url : thb}
+              });
+              slides.push(slide);
+              console.log(slide);
+              this.sceneView.atmosphere ='high'
+              this.sceneView.map.presentation.slides.push(slide)
+              });
       var i =0;
       document
             .getElementById('gotoview').addEventListener("click",  () =>{
-              this.sceneView.goTo(slides[i])
+              /*this.sceneView.goTo(slides[i])
               if (i < slides.length-1){
                 i++;}
-              else{i=0}});
+              else{i=0}*/
+              slides[i].applyTo(this.sceneView)
+              if (i < slides.length-1){
+                i++;}
+              else{i=0}
+            });
 
 
       console.log('startup');
@@ -101,6 +158,8 @@ function(declare, BaseWidget,Camera) {
 
     onOpen: function(){
       console.log('onOpen');
+      console.log(this.sceneView.map.presentation.slides);
+
     },
 
     onClose: function(){
